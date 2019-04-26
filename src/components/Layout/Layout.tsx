d14a084a6941bb4c0936bc130bd31e9fb1a2dcb2
Angel S. Moreno <angelxmoreno@gmail.com>
2019-04-26 12:57:51 -0400

    code clean up

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,17 +1,6 @@
 import React, { ReactNode } from "react";
 
 import {
-  Button,
-  Navbar,
-  Container,
-  Row,
-  Col,
-  NavbarBrand,
-  NavLink,
-  NavItem,
-  NavbarToggler,
-  UncontrolledDropdown,
-  DropdownToggle,
   Fade
 } from "reactstrap";
 import Navigation from "./Navigation";
@@ -29,7 +18,7 @@ const Layout: React.SFC<LayoutProps> = ({ title, children, headerContent }) => (
     <div className="container-fluid">
       <div className="row">
         <Sidebar />
-        <Fade fadeOut={true} tag="main" role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
+        <Fade in={true} tag="main" role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
           <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
             <h1 className="h2">{title}</h1>
             {headerContent}
